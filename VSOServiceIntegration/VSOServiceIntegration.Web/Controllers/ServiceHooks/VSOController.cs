683d4ebb23f91a4df59d9d536abbeb8c9dcd518b
mblanchard <matthewablanchard@gmail.com>
2014-10-21 15:23:09 -0500

    Added VSOController

---
+++ VSOServiceIntegration/VSOServiceIntegration.Web/Controllers/ServiceHooks/VSOController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace VSOServiceIntegration.Web.Controllers.ServiceHooks
+{
+    public class VSOController : ApiController
+    {
+        [System.Web.Mvc.HttpPost]
+        public ActionResult ServiceHook([FromBody] string value)
+        {
+            return new HttpStatusCodeResult(200, value);
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult ServiceHook()
+        {
+            return new HttpStatusCodeResult(200);
+        }
+
+
+        [System.Web.Mvc.HttpGet]
+        public ActionResult CheckHistory()
+        {
+            return new HttpStatusCodeResult(200, "History");
+        }
+    }
+}
